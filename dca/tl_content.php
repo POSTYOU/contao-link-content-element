70dd9c7fe765d0886035c9ac5c657a2daded4ab7
Gerald Meier <garyee@gmx.de>
2015-08-05 15:51:56 +0200

    subsubpalette fix

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -2,25 +2,26 @@
 
 foreach ($GLOBALS['TL_DCA']['tl_content']['palettes'] as $key=>$palette){ 
     if(!is_array($palette) && is_string($palette)) {
-            $GLOBALS['TL_DCA']['tl_content']['palettes'][$key] = preg_replace('/;/', ';{boxLink_legend:hide},addLinkContentElement;', $GLOBALS['TL_DCA']['tl_content']['palettes'][$key],1);
+        //if(preg_match('/boxLink/',$GLOBALS['TL_DCA']['tl_content']['palettes'][$key])==0)
+            $GLOBALS['TL_DCA']['tl_content']['palettes'][$key] = preg_replace('/;/', ';{boxLink_legend:hide},addLinkContentElement,elementHref_cursor,elementHref_target,hrefType;', $GLOBALS['TL_DCA']['tl_content']['palettes'][$key],1);
     }
 }
 
-// add Selector
-$GLOBALS['TL_DCA']['tl_content']['palettes']['__selector__'][] = 'addLinkContentElement';
+//$GLOBALS['TL_DCA']['tl_content']['palettes']['__selector__'][] = 'addLinkContentElement';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['__selector__'][] = 'hrefType';
 
 // add Subpalettes
-$GLOBALS['TL_DCA']['tl_content']['subpalettes']['addLinkContentElement'] = 'elementHref_cursor,elementHref_target,hrefType';
-$GLOBALS['TL_DCA']['tl_content']['subpalettes']['hrefType_elementHrefLink'] = 'elementHrefTitle,elementHrefLink';
-$GLOBALS['TL_DCA']['tl_content']['subpalettes']['hrefType_elementHrefFile'] = 'elementHrefTitle,elementHrefFile';
+$GLOBALS['TL_DCA']['tl_content']['subpalettes']['hrefType_0'] = 'elementHrefTitle,elementHrefLink';
+$GLOBALS['TL_DCA']['tl_content']['subpalettes']['hrefType_1'] = 'elementHrefTitle,elementHrefFile';
 
 
 // Add fields
 $GLOBALS['TL_DCA']['tl_content']['fields']['addLinkContentElement'] = array
 (
+
+	//'reference' => &$GLOBALS['TL_LANG']['tl_content'],
     'label'				=> &$GLOBALS['TL_LANG']['tl_content']['addLinkContentElement'],
-    'exclude'			=> true,
+    //'exclude'			=> true,
     'inputType'			=> 'checkbox',
     'eval'				=> array('submitOnChange'=>true, 'tl_class' => 'clr'),
     'sql'				=> "char(1) NOT NULL default ''"
@@ -47,12 +48,12 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['elementHref_target'] = array
 $GLOBALS['TL_DCA']['tl_content']['fields']['hrefType'] = array
 (
 'label' => &$GLOBALS['TL_LANG']['tl_content']['hrefType'],
-'default' => 'elementHrefFile',
+'default' => '1',
 'inputType' => 'radio',
-'options_callback' => array('LinkContent_tl_content', 'getSubPalettesOptions'),
+'options' => array(0,1),
 'reference' => &$GLOBALS['TL_LANG']['tl_content'],
-'eval' => array('submitOnChange'=>true,'mandatory'=>true,'tl_class'=>'clr'),
-'sql' => "varchar(256) NOT NULL default ''"
+'eval' => array('submitOnChange'=>true,'tl_class'=>'clr'),
+'sql' => "varchar(1) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['elementHrefTitle'] = array
@@ -68,7 +69,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['elementHrefLink'] = array
 (
     'label'                   => &$GLOBALS['TL_LANG']['tl_content']['elementHrefLink'],
     'inputType'               => 'text',
-    'eval'                    => array('rgxp'=>'url', 'decodeEntities'=>true,'mandatory'=>true, 'maxlength'=>255, 'fieldType'=>'radio', 'tl_class'=>'clr wizard'),
+    'eval'                    => array('rgxp'=>'url', 'decodeEntities'=>true, 'maxlength'=>255, 'fieldType'=>'radio', 'tl_class'=>'clr wizard'),
     'wizard' => array
     (
         array('tl_content', 'pagePicker')
@@ -81,19 +82,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['elementHrefFile'] = array
     'label'				=> &$GLOBALS['TL_LANG']['tl_content']['elementHrefFile'],
     'exclude'	=> true,
     'inputType'	=> 'fileTree',
-    'eval'	=> array('filesOnly'=>true, 'fieldType'=>'radio','mandatory'=>true, 'tl_class'=>'clr'),
+    'eval'	=> array('filesOnly'=>true, 'fieldType'=>'radio', 'tl_class'=>'clr'),
     'sql'	=> "binary(16) NULL"
 );
-
-
-class LinkContent_tl_content{
-
-    function getSubPalettesOptions(){
-        return array('elementHrefLink', 'elementHrefFile');
-    }
-
-
-
-}
-
-
