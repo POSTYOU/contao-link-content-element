ebac36f1ad38100b2c933faa4daeab6269324cc4
Gerald Meier <garyee@gmx.de>
2015-08-05 15:52:25 +0200

    subsubpalette fix

--- languages/de/tl_content.php
+++ languages/de/tl_content.php
@@ -9,9 +9,11 @@ $GLOBALS['TL_LANG']['tl_content']['addLinkContentElement'][1]= "";
 $GLOBALS['TL_LANG']['tl_content']['elementHrefTitle'][0]="Link Beschreibungstext";
 $GLOBALS['TL_LANG']['tl_content']['elementHrefTitle'][1]="Link Text Beschreibung";
 
+$GLOBALS['TL_LANG']['tl_content']['1'][0]="Datei";
 $GLOBALS['TL_LANG']['tl_content']['elementHrefFile'][0]="Datei";
 $GLOBALS['TL_LANG']['tl_content']['elementHrefFile'][1]="Das Element verlinkt zu einer Datei";
 
+$GLOBALS['TL_LANG']['tl_content']['0'][0]="Url";
 $GLOBALS['TL_LANG']['tl_content']['elementHrefLink'][0]="Url";
 $GLOBALS['TL_LANG']['tl_content']['elementHrefLink'][1]="Das Element verlinkt zu einer Website";
 
