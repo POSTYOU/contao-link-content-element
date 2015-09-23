0cae2278a8184760d1b1ef70fb0bbf1496167e62
Gerald Meier <garyee@gmx.de>
2015-09-23 09:01:33 +0200

    Update tl_content.php
    file/url distinction obsolete

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -3,16 +3,14 @@
 foreach ($GLOBALS['TL_DCA']['tl_content']['palettes'] as $key=>$palette){ 
     if(!is_array($palette) && is_string($palette)) {
         //if(preg_match('/boxLink/',$GLOBALS['TL_DCA']['tl_content']['palettes'][$key])==0)
-            $GLOBALS['TL_DCA']['tl_content']['palettes'][$key] = preg_replace('/;/', ';{boxLink_legend:hide},addLinkContentElement,elementHref_cursor,elementHref_target,hrefType;', $GLOBALS['TL_DCA']['tl_content']['palettes'][$key],1);
+            $GLOBALS['TL_DCA']['tl_content']['palettes'][$key] = preg_replace('/;/', ';{boxLink_legend:hide},addLinkContentElement;', $GLOBALS['TL_DCA']['tl_content']['palettes'][$key],1);
     }
 }
 
-//$GLOBALS['TL_DCA']['tl_content']['palettes']['__selector__'][] = 'addLinkContentElement';
-$GLOBALS['TL_DCA']['tl_content']['palettes']['__selector__'][] = 'hrefType';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['__selector__'][] = 'addLinkContentElement';
 
 // add Subpalettes
-$GLOBALS['TL_DCA']['tl_content']['subpalettes']['hrefType_0'] = 'elementHrefTitle,elementHrefLink';
-$GLOBALS['TL_DCA']['tl_content']['subpalettes']['hrefType_1'] = 'elementHrefTitle,elementHrefFile';
+$GLOBALS['TL_DCA']['tl_content']['subpalettes']['addLinkContentElement'] = 'elementHref_cursor,elementHref_target,elementHrefTitle,elementHrefLink';
 
 
 // Add fields
@@ -45,17 +43,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['elementHref_target'] = array
     'sql'                     => "char(1) NOT NULL default ''"
 );
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['hrefType'] = array
-(
-'label' => &$GLOBALS['TL_LANG']['tl_content']['hrefType'],
-'default' => '1',
-'inputType' => 'radio',
-'options' => array(0,1),
-'reference' => &$GLOBALS['TL_LANG']['tl_content'],
-'eval' => array('submitOnChange'=>true,'tl_class'=>'clr'),
-'sql' => "varchar(1) NOT NULL default ''"
-);
-
 $GLOBALS['TL_DCA']['tl_content']['fields']['elementHrefTitle'] = array
 (
     'label'                   => &$GLOBALS['TL_LANG']['tl_content']['elementHrefTitle'],
@@ -69,19 +56,10 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['elementHrefLink'] = array
 (
     'label'                   => &$GLOBALS['TL_LANG']['tl_content']['elementHrefLink'],
     'inputType'               => 'text',
-    'eval'                    => array('rgxp'=>'url', 'decodeEntities'=>true, 'maxlength'=>255, 'fieldType'=>'radio', 'tl_class'=>'clr wizard'),
+    'eval'                    => array('rgxp'=>'url','filesOnly'=>true, 'decodeEntities'=>true, 'maxlength'=>255, 'fieldType'=>'radio', 'tl_class'=>'clr wizard'),
     'wizard' => array
     (
         array('tl_content', 'pagePicker')
     ),
     'sql'                     => "varchar(255) NOT NULL default ''"
-);
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['elementHrefFile'] = array
-(
-    'label'				=> &$GLOBALS['TL_LANG']['tl_content']['elementHrefFile'],
-    'exclude'	=> true,
-    'inputType'	=> 'fileTree',
-    'eval'	=> array('filesOnly'=>true, 'fieldType'=>'radio', 'tl_class'=>'clr'),
-    'sql'	=> "binary(16) NULL"
 );
