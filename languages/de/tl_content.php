8d332edc717bb4070c325a62431c3a3500733a48
theDyingMountain <mario_gates@ymail.com>
2017-04-07 14:25:54 +0200

    Sprach/Rechtschreiboptimierung

--- languages/de/tl_content.php
+++ languages/de/tl_content.php
@@ -3,7 +3,7 @@
 $GLOBALS['TL_LANG']['tl_content']['boxLink_legend'] ="Element-Link";
 $GLOBALS['TL_LANG']['tl_content']['elementHref_cursor'] =array("cursor:pointer","Der Cursor wird verändert");
 
-$GLOBALS['TL_LANG']['tl_content']['addLinkContentElement'][0]= "Dem Element ein Link hinzufügen";
+$GLOBALS['TL_LANG']['tl_content']['addLinkContentElement'][0]= "Dem Element einen Link hinzufügen";
 $GLOBALS['TL_LANG']['tl_content']['addLinkContentElement'][1]= "";
 
 $GLOBALS['TL_LANG']['tl_content']['elementHrefTitle'][0]="Link Beschreibungstext";
@@ -11,12 +11,11 @@ $GLOBALS['TL_LANG']['tl_content']['elementHrefTitle'][1]="Link Text Beschreibung
 
 $GLOBALS['TL_LANG']['tl_content']['1'][0]="Datei";
 $GLOBALS['TL_LANG']['tl_content']['elementHrefFile'][0]="Datei";
-$GLOBALS['TL_LANG']['tl_content']['elementHrefFile'][1]="Das Element verlinkt zu einer Datei";
+$GLOBALS['TL_LANG']['tl_content']['elementHrefFile'][1]="Das Element verlinkt zu einer Datei.";
 
 $GLOBALS['TL_LANG']['tl_content']['0'][0]="Url";
 $GLOBALS['TL_LANG']['tl_content']['elementHrefLink'][0]="Url";
-$GLOBALS['TL_LANG']['tl_content']['elementHrefLink'][1]="Das Element verlinkt zu einer Website";
+$GLOBALS['TL_LANG']['tl_content']['elementHrefLink'][1]="Das Element verlinkt zu einer Website.";
 
 $GLOBALS['TL_LANG']['tl_content']['hrefType'][0]="Link Typ";
-$GLOBALS['TL_LANG']['tl_content']['hrefType'][1]="wählen sie einen Link-Typen aus";
-?>
+$GLOBALS['TL_LANG']['tl_content']['hrefType'][1]="Wählen sie einen Link-Typen aus";
