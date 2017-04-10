1f6c99313c147fb8fac1d3eb0541d8a79cdc152e
theDyingMountain <mario_gates@ymail.com>
2017-04-10 15:55:07 +0200

    escape quotes in title attribute

--- classes/LinkContentElement.php
+++ classes/LinkContentElement.php
@@ -12,10 +12,9 @@
  */
 namespace postyou;
 
-
 class LinkContentElement extends \Contao\Frontend
 {
-    function getContentElementHook($objElement, $strBuffer)
+    public function getContentElementHook($objElement, $strBuffer)
     {
         $replaceTarget="_self";
         $replaceStyle="";
@@ -23,26 +22,27 @@ class LinkContentElement extends \Contao\Frontend
         $replaceURL=null;
 
         if (isset($objElement->row()["addLinkContentElement"]) && $objElement->row()["addLinkContentElement"]==="1") {
-
-            if (isset($objElement->row()["elementHrefLink"]) && !empty($objElement->row()["elementHrefLink"]))
+            if (isset($objElement->row()["elementHrefLink"]) && !empty($objElement->row()["elementHrefLink"])) {
                 $replaceURL =$objElement->row()["elementHrefLink"];
+            }
 
-            if (isset($objElement->row()["elementHref_target"]) && $objElement->row()["elementHref_target"]==="1")
+            if (isset($objElement->row()["elementHref_target"]) && $objElement->row()["elementHref_target"]==="1") {
                 $replaceTarget="_blank";
+            }
 
-            if (isset($objElement->row()["elementHref_cursor"]) && $objElement->row()["elementHref_cursor"]==="1")
+            if (isset($objElement->row()["elementHref_cursor"]) && $objElement->row()["elementHref_cursor"]==="1") {
                 $replaceStyle="style=\"cursor:pointer\"";
+            }
 
             $onclick="";
-            if(isset($replaceURL)){
-				if (strpos($replaceURL, '{{') !== false)
+            if (isset($replaceURL)) {
+                if (strpos($replaceURL, '{{') !== false) {
                     $replaceURL="/".$replaceURL;
+                }
                 $onclick="onclick=\"window.open('". $replaceURL."','".$replaceTarget."');\"";
-			}
-            $strBuffer = preg_replace("/div/", "div ".$replaceStyle."  title=\"".$objElement->row()["elementHrefTitle"]."\" ".$onclick, $strBuffer, 1);
+            }
             $strBuffer = preg_replace("/class=\"/", "class=\"" .$replaceClass." ", $strBuffer, 1);
         }
         return $strBuffer;
-
     }
 }
